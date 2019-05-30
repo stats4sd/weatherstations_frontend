fba538ff8528aa174c640674ec05405b82f078f8
luciafalcinelli <lucia@stats4sd.org>
2019-05-30 18:46:47 +0100

    charts added to dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -3,7 +3,7 @@
 @section('header')
     <section class="content-header">
       <h1>
-        {{ trans('backpack::base.dashboard') }}<small>{{ trans('backpack::base.first_page_you_see') }}</small>
+        {{ trans('backpack::base.dashboard') }}
       </h1>
       <ol class="breadcrumb">
         <li><a href="{{ backpack_url() }}">{{ config('backpack.base.project_name') }}</a></li>
@@ -18,24 +18,59 @@
         <div class="col-md-12">
             <div class="box">
                 <div class="box-header with-border">
-                    <div class="box-title">{{ trans('backpack::base.login_status') }}</div>
+                    <div class="box-title">
+                        <label>Daily Aggregation Charts</label>
+                    </div>
                 </div>
 
                 <div class="box-body">
                     <div class="container-fluid">
                         <div class="row">
-                            <div class="col-sm-12">
-                                <div class="card-body">
-                                    <div id="temps_div_in"></div>
-                                    <?= $lava_temp_in->render('LineChart', 'TemperatureIn', 'temps_div_in') ?>  
 
-                        
-                                </div>
+                            <div class="col-sm-12">
+                                <div id="temps_div_in"></div>
+                                    <?= Lava::render('LineChart', 'TemperatureIn', 'temps_div_in') ?>  
                             </div>
-                       
-               
-                            <div id="temps_div_out"></div>
-                                <?= $lava_temp_out->render('LineChart', 'TemperatureOut', 'temps_div_out') ?> 
+
+                            <div class="col-sm-12">
+                                <div id="temps_div_out"></div>
+                                <?= Lava::render('LineChart', 'TemperatureOut', 'temps_div_out') ?>  
+                            </div>
+
+                            <div class="col-sm-12">
+                                <div id="lava_hum_in"></div>
+                                <?= Lava::render('LineChart', 'HumedadIn', 'lava_hum_in') ?>  
+                            </div>
+
+                            <div class="col-sm-12">
+                                <div id="lava_hum_out"></div>
+                                <?= Lava::render('LineChart', 'HumedadOut', 'lava_hum_out') ?>  
+                            </div>
+
+                             <div class="col-sm-12">
+                                <div id="pres_rel"></div>
+                                <?= Lava::render('LineChart', 'PressionR', 'pres_rel') ?>  
+                            </div>
+
+                            <div class="col-sm-12">
+                                <div id="pres_abs"></div>
+                                <?= Lava::render('LineChart', 'PressionA', 'pres_abs') ?>  
+                            </div>
+
+                            <div class="col-sm-12">
+                                <div id="veloc_viento"></div>
+                                <?= Lava::render('LineChart', 'VelocidadViento', 'veloc_viento') ?>  
+                            </div>
+
+                            <div class="col-sm-12">
+                                <div id="sens_termica"></div>
+                                <?= Lava::render('LineChart', 'SensTermica', 'sens_termica') ?>  
+                            </div>
+
+
+                            
+
+
                         
                         </div>
                     </div>
@@ -43,7 +78,7 @@
             </div>
         </div>
     </div>
-    
+
 
 
 
