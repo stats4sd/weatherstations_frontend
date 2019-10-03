dd8fa66165e2ab9b0c83872d29e4dfb257d8d582
luciafalcinelli <lucia@stats4sd.org>
2019-10-03 17:01:55 +0100

    dashboard yearly for memory problem

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Daily;
+use App\Yearly;
 use App\Dashboard;
 use DB;
 use Illuminate\Http\Request;
@@ -15,14 +16,14 @@ class DashboardController extends Controller
     public function index ()
 
     {
-        $station_data = DB::table('data')->get();
+        $station_data = DB::table('yearly_data')->get();
         $stations = DB::table('stations')->get();
        
 
 
         $lava_temp_in= new Lavacharts;
         if(!empty($station_data)){
-            $data=Daily::select("fecha as 0","max_temperatura_interna as 1", "avg_temperatura_interna as 2","min_temperatura_interna as 3")->get()->toArray();
+            $data=Yearly::select("fecha as 0","max_temperatura_interna as 1", "avg_temperatura_interna as 2","min_temperatura_interna as 3")->get()->toArray();
             $datatable = Lava::DataTable();
 
             $datatable -> addDateColumn('Date')
@@ -32,13 +33,13 @@ class DashboardController extends Controller
                         -> addRows($data);
 
             Lava::LineChart('TemperatureIn', $datatable,[
-                    'title' => 'DailyTemperatures Interna °C'                 
+                    'title' => 'Yearly Temperatures Interna °C'                 
             ]);
         }
 
         $lava_temp_out= new Lavacharts;
         if(!empty($station_data)){
-            $data=Daily::select("fecha as 0","max_temperatura_externa as 1", "min_temperatura_externa as 2","avg_temperatura_externa as 3")->get()->toArray();
+            $data=Yearly::select("fecha as 0","max_temperatura_externa as 1", "min_temperatura_externa as 2","avg_temperatura_externa as 3")->get()->toArray();
             $datatable = Lava::DataTable();
 
             $datatable -> addDateColumn('Date')
@@ -48,13 +49,13 @@ class DashboardController extends Controller
                         -> addRows($data);
 
             Lava::LineChart('TemperatureOut', $datatable,[
-                    'title' => 'DailyTemperatures Externa °C'                 
+                    'title' => 'Yearly Temperatures Externa °C'                 
             ]);
         }
 
         $lava_hum_in= new Lavacharts;
         if(!empty($station_data)){
-            $data=Daily::select("fecha as 0","max_humedad_interna as 1", "min_humedad_interna as 2","avg_humedad_interna as 3")->get()->toArray();
+            $data=Yearly::select("fecha as 0","max_humedad_interna as 1", "min_humedad_interna as 2","avg_humedad_interna as 3")->get()->toArray();
             $datatable = Lava::DataTable();
 
             $datatable -> addDateColumn('Date')
@@ -64,13 +65,13 @@ class DashboardController extends Controller
                         -> addRows($data);
 
             Lava::LineChart('HumedadIn', $datatable,[
-                    'title' => 'DailyHumedad Interna %'                 
+                    'title' => 'Yearly Humedad Interna %'                 
             ]);
         }
 
         $lava_hum_out= new Lavacharts;
         if(!empty($station_data)){
-            $data=Daily::select("fecha as 0","max_humedad_externa as 1", "min_humedad_externa as 2","avg_humedad_externa as 3")->get()->toArray();
+            $data=Yearly::select("fecha as 0","max_humedad_externa as 1", "min_humedad_externa as 2","avg_humedad_externa as 3")->get()->toArray();
             $datatable = Lava::DataTable();
 
             $datatable -> addDateColumn('Date')
@@ -80,13 +81,13 @@ class DashboardController extends Controller
                         -> addRows($data);
 
             Lava::LineChart('HumedadOut', $datatable,[
-                    'title' => 'DailyHumedad Externa %'                 
+                    'title' => 'Yearly Humedad Externa %'                 
             ]);
         }
 
         $lava_hum_out= new Lavacharts;
         if(!empty($station_data)){
-            $data=Daily::select("fecha as 0","max_presion_relativa as 1", "min_presion_relativa as 2","avg_presion_relativa as 3")->get()->toArray();
+            $data=Yearly::select("fecha as 0","max_presion_relativa as 1", "min_presion_relativa as 2","avg_presion_relativa as 3")->get()->toArray();
             $datatable = Lava::DataTable();
 
             $datatable -> addDateColumn('Date')
@@ -96,13 +97,13 @@ class DashboardController extends Controller
                         -> addRows($data);
 
             Lava::LineChart('PressionR', $datatable,[
-                    'title' => 'DailyPresion Relativa hPa'                 
+                    'title' => 'Yearly Presion Relativa hPa'                 
             ]);
         }
 
         $lava_hum_out= new Lavacharts;
         if(!empty($station_data)){
-            $data=Daily::select("fecha as 0","max_presion_absoluta as 1", "min_presion_absoluta as 2","avg_presion_absoluta as 3")->get()->toArray();
+            $data=Yearly::select("fecha as 0","max_presion_absoluta as 1", "min_presion_absoluta as 2","avg_presion_absoluta as 3")->get()->toArray();
             $datatable = Lava::DataTable();
 
             $datatable -> addDateColumn('Date')
@@ -112,13 +113,13 @@ class DashboardController extends Controller
                         -> addRows($data);
 
             Lava::LineChart('PressionA', $datatable,[
-                    'title' => 'DailyPresion Absoluta hPa'                 
+                    'title' => 'Yearly Presion Absoluta hPa'                 
             ]);
         }
 
         $lava_hum_out= new Lavacharts;
         if(!empty($station_data)){
-            $data=Daily::select("fecha as 0","max_velocidad_viento as 1", "min_velocidad_viento as 2","avg_velocidad_viento as 3")->get()->toArray();
+            $data=Yearly::select("fecha as 0","max_velocidad_viento as 1", "min_velocidad_viento as 2","avg_velocidad_viento as 3")->get()->toArray();
             $datatable = Lava::DataTable();
 
             $datatable -> addDateColumn('Date')
@@ -128,13 +129,13 @@ class DashboardController extends Controller
                         -> addRows($data);
 
             Lava::LineChart('VelocidadViento', $datatable,[
-                    'title' => 'DailyVelocidad Viento m/s'                 
+                    'title' => 'Yearly Velocidad Viento m/s'                 
             ]);
         }
 
         $lava_hum_out= new Lavacharts;
         if(!empty($station_data)){
-            $data=Daily::select("fecha as 0","max_sensacion_termica as 1", "min_sensacion_termica as 2","avg_sensacion_termica as 3")->get()->toArray();
+            $data=Yearly::select("fecha as 0","max_sensacion_termica as 1", "min_sensacion_termica as 2","avg_sensacion_termica as 3")->get()->toArray();
             $datatable = Lava::DataTable();
 
             $datatable -> addDateColumn('Date')
@@ -144,7 +145,7 @@ class DashboardController extends Controller
                         -> addRows($data);
 
             Lava::LineChart('SensTermica', $datatable,[
-                    'title' => 'DailySensacion Termica °C'                 
+                    'title' => 'Yearly Sensacion Termica °C'                 
             ]);
         }
 
