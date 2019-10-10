f1090244314cfc5718f102acac8c5a7c4d0b7200
luciafalcinelli <lucia@stats4sd.org>
2019-10-10 19:03:10 +0100

    delete

---
+++ resources/views/vendor/backpack/crud/buttons/deleteByFilters.blade.php
@@ -0,0 +1,50 @@
+
+@if ($crud->hasAccess('delete'))
+<a href="javascript:void(0)" onclick="getFilter(event)" class="btn btn-sm btn-secondary" data-toggle="popover" id='deletebyfilter'><i class="fa fa-ban"></i> delete by filters</a>
+@endif
+
+
+<script>
+	if(typeof getFilter != 'function') {
+	
+		
+		/**
+		 * Gets the currently visible columns, any user-added ordering, and applied filters to the crud table, then sends that all to the downloadFromCrud route for server-side preparation of the download
+		 * @param  {event} e The event that occured. Only used to get the target of the event (the button) so we can disable it during processing.
+		 * @return {null}   Result of function is not returned - but the user is redirected to the download url once the file has been generated.
+		 */
+      	function getFilter(e) {
+      		var target = e.target;
+
+      		target.disabled = true;
+			target.innerHTML = `<div class="spinner-border spinner-border-sm"></div> Preparing...`;
+	      	// get user-specified parameters
+			var columnsVisible = crud.table.columns().visible().toArray();
+			var order = crud.table.order()
+			
+			var postObj = {
+				
+				'order':order,
+			}
+			$.ajax({
+				"url": "{{ url($crud->route . '/deleteByFilters') }}",
+				"method":"POST",
+				"data": postObj,
+				"success": function(result) {
+			
+					console.log("success");
+					// window.open(result,'_blank');
+				},
+				"error": function(result){
+				
+					console.log("error");
+				},
+				"complete": function() {
+					target.disabled = false;
+					target.innerHTML = "<i class='fa fa-ban'></i> Data Deleted ";
+				}
+			})
+		}
+	 
+	}
+</script>
