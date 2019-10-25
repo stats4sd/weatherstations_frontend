5c893ac3d732fecfa660eae7fd0ee7805f05e087
luciafalcinelli <lucia@stats4sd.org>
2019-10-25 11:51:49 +0100

    views after update tables

---
+++ resources/views/eventListener.blade.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport"
+		  content="width=device-width, user-scalable=no, initial-scale=1.0, minimum-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<meta name="csrf-token" content="{{ csrf_token() }}">
+	<title>Listener</title>
+</head>
+<body>
+<div id='app'>
+	<p>Event Listener page</p>
+	
+</div>
+<script src="js/app.js" charset="utf-8" type="text/javascript">
+	
+</script>
+</body>	
+</html>
