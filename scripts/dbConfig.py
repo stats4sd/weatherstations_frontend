f9e8fd0d6817662a93e2eba22b15d6eac4862053
luciafalcinelli <lucia@stats4sd.org>
2019-10-25 18:44:06 +0100

    add details database

--- scripts/dbConfig.py
+++ scripts/dbConfig.py
@@ -1,9 +1,9 @@
 
 dbConfig = {
-            'user': 'root',
-            'password': 'Logoslogos88',
-            'host': '127.0.0.1',
-            'db': 'umsa'
+            'user': 'weatherstations',
+            'password': 'stations26443',
+            'host': 'staging-weatherstations-ccrp.stats4sd.org',
+            'db': 'staging_weatherstations'
             }
 
 list_columns_davis = ['hi_temp', 'low_temp', 'wind_cod',
