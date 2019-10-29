026a8eaf829f67c0d0b5e8c52bcb886a8042feb5
luciafalcinelli <lucia@stats4sd.org>
2019-10-29 16:26:29 +0000

    added ;

--- app/Http/Controllers/Admin/DataCrudController.php
+++ app/Http/Controllers/Admin/DataCrudController.php
@@ -205,7 +205,7 @@ class DataCrudController extends CrudController
         $query = '"'.$query.'"';
         $params = '"'.$params.'"';
         $file_name = date('mdY')."data.csv";
-        echo($query)
+        echo($query);
 
         
         //python script accepts 7 arguments in this order: db_user db_password db_name base_path() query params
