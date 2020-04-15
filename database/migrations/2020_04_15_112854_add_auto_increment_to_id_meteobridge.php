c2aef90cd273638c8c48ad5e10c331ed4f72f2bb
luciafalcinelli <44463646+luciafalcinelli@users.noreply.github.com>
2020-04-15 15:12:57 +0100

    add autoincrement to id into meteobridge

--- database/migrations/2020_04_15_112854_add_auto_increment_to_id_meteobridge.php
+++ database/migrations/2020_04_15_112854_add_auto_increment_to_id_meteobridge.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AddAutoIncrementToId extends Migration
+class AddAutoIncrementToIdMeteoBridge extends Migration
 {
     /**
      * Run the migrations.
