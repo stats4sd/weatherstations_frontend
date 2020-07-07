23dc757affc6d3a6e1f4f1ab22d6def892e2b9e9
luciafalcinelli <44463646+luciafalcinelli@users.noreply.github.com>
2020-07-07 12:31:19 +0100

    added the departimento_id and municipio_id

---
+++ database/migrations/2020_07_07_112721_add_departamento_id_to_municipios_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddDepartamentoIdToMunicipiosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('municipio', function (Blueprint $table) {
+            $table->bigIncrements('departamento_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('municipio', function (Blueprint $table) {
+            //
+        });
+    }
+}
---
+++ database/migrations/2020_07_07_113017_add_municipio_id_to_comunidad_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddMunicipioIdToComunidadTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('comunidad', function (Blueprint $table) {
+            $table->bigIncrements('municipio_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('comunidad', function (Blueprint $table) {
+            //
+        });
+    }
+}
