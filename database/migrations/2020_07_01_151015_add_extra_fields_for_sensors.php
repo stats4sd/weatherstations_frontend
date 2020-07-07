3a40fdce6a4a2cb67ecd6a07bb4f8ce76f0a3cc3
luciafalcinelli <44463646+luciafalcinelli@users.noreply.github.com>
2020-07-07 12:16:15 +0100

    fixed problem with add_extra_fields migration

--- database/migrations/2020_07_01_151015_add_extra_fields_for_sensors.php
+++ database/migrations/2020_07_01_151015_add_extra_fields_for_sensors.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AddExtraFiledsForSensors extends Migration
+class AddExtraFieldsForSensors extends Migration
 {
     /**
      * Run the migrations.
