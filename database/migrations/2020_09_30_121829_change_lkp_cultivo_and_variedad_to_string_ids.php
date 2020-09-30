fa15faa84dd821ff5401121104d27e7f1c1b5651
Dave Mills <d.e.mills@stats4sd.org>
2020-09-30 13:24:57 +0100

    updating cultivos and variedades lookup tables to have string ids - to match ODK form

---
+++ database/migrations/2020_09_30_121829_change_lkp_cultivo_and_variedad_to_string_ids.php
@@ -0,0 +1,58 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class ChangeLkpCultivoAndVariedadToStringIds extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('cultivos', function (Blueprint $table) {
+            $table->string('lkp_cultivo_id')->change();
+            $table->string('lkp_variedad_id')->change();
+        });
+
+        Schema::table('fenologia', function (Blueprint $table) {
+            $table->dropColumn('variedad_id');
+        });
+
+        Schema::table('lkp_cultivos', function (Blueprint $table) {
+            $table->string('id')->change();
+        });
+
+        Schema::table('lkp_variedades', function (Blueprint $table) {
+            $table->string('id')->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('cultivos', function (Blueprint $table) {
+            $table->unsignedBigInteger('lkp_cultivo_id')->change();
+            $table->unsignedBigInteger('lkp_variedad_id')->change();
+        });
+
+        Schema::table('fenologia', function (Blueprint $table) {
+            $table->unsignedBigInteger('variedad_id');
+        });
+
+        Schema::table('lkp_cultivos', function (Blueprint $table) {
+            $table->bigIncrements('id')->primary()->change();
+        });
+
+        Schema::table('lkp_variedades', function (Blueprint $table) {
+            $table->bigIncrements('id')->primary()->change();
+        });
+    }
+}
