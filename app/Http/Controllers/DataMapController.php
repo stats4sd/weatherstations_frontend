05a8b7e8be4dda157a46211cedde3173d3e46abb
luciafalcinelli <44463646+luciafalcinelli@users.noreply.github.com>
2020-10-01 11:25:35 +0100

    added the variable name into NewDataVariableSpotted Event

--- app/Http/Controllers/DataMapController.php
+++ app/Http/Controllers/DataMapController.php
@@ -54,7 +54,7 @@ class DataMapController extends Controller
             if($variable['in_db'] == 0) {
                 //don't actually process it (as the SQL Insert will fail)
                 //just tell the admin about it!
-                NewDataVariableSpotted::dispatch();
+                NewDataVariableSpotted::dispatch($variable['name']);
                 continue;
             }
 
