3091f69ad891e1338a3c6b4dfd3d86d1baf03712
Dave Mills <d.e.mills@stats4sd.org>
2020-10-13 15:37:39 +0100

    tidying up migration for datamaps select_multiple fields

--- database/migrations/2020_10_09_120543_add_multiple_to_data_maps_table.php
+++ database/migrations/2020_10_09_120543_add_multiple_to_data_maps_table.php
@@ -15,9 +15,11 @@ class AddMultipleToDataMapsTable extends Migration
     {
         Schema::table('data_maps', function (Blueprint $table) {
             $table->string('select_multiple')->nullable()->comment('if this is set, this data map is for data at a different level, based on a select_multiple and possibly a repeat group based on that select_multiple response.');
-            $table->string('repeat_group')->nullable()->comment('the name of the repeat group to look inside to find the main variables for this data map');
             $table->string('select_multiple_other')->nullable()->comment('the name of the "enter other value" question linked to the select_multiple variable');
-            $table->string('inner_name')->nullable()->comment('the jr:choice-name() calculate field that is inside the repeat group. This is used so that we know where to place the "other" name in case there is an "other" option in the select_multiple');
+            $table->string('select_multiple_other_label')->nullable()->comment('the variable name that contains the "specify other" question. (This is so we can pull in the user-specified label)');
+            $table->string('repeat_group')->nullable()->comment('the name of the repeat group to look inside to find the main variables for this data map');
+            $table->string('inner_name')->nullable()->comment('the variable name of the calculate with the `selected-at(pos(..))` code (inside the repeat group)');
+            $table->string('innter_label')->nullable()->comment('the variable name of the calculate with the `jr:choice-name()` code (inside the repeat group)');
         });
     }
 
@@ -30,7 +32,11 @@ class AddMultipleToDataMapsTable extends Migration
     {
         Schema::table('data_maps', function (Blueprint $table) {
             $table->dropColumn('select_multiple');
+            $table->dropColumn('select_multiple_other');
+            $table->dropColumn('select_multiple_other_label');
             $table->dropColumn('repeat_group');
+            $table->dropColumn('inner_name');
+            $table->dropColumn('innter_label');
         });
     }
 }
