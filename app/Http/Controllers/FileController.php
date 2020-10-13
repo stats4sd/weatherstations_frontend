20f22e6029456202a43a7cada9fcfe25f064451f
luciafalcinelli <44463646+luciafalcinelli@users.noreply.github.com>
2020-10-13 18:20:23 +0100

    uncommeted the setTimeout

--- app/Http/Controllers/FileController.php
+++ app/Http/Controllers/FileController.php
@@ -70,7 +70,7 @@ class FileController extends Controller
 
             $process = new Process("pipenv run python3 {$scriptPath} {$path_name} {$station} {$request->selectedUnitTemp} {$request->selectedUnitPres} {$request->selectedUnitWind} {$request->selectedUnitRain} {$uploader_id}");
 
-            $process->setTimeout(500);
+            // $process->setTimeout(500);
 
             $process->run();
 
