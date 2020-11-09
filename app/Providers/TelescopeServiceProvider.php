52a61c2348372ecd32b381cbe4fcd322277c1aa3
luciafalcinelli <44463646+luciafalcinelli@users.noreply.github.com>
2020-11-09 16:21:51 +0000

    allows telescope to see the logs

--- app/Providers/TelescopeServiceProvider.php
+++ app/Providers/TelescopeServiceProvider.php
@@ -21,7 +21,7 @@ class TelescopeServiceProvider extends TelescopeApplicationServiceProvider
         $this->hideSensitiveRequestDetails();
 
         Telescope::filter(function (IncomingEntry $entry) {
-            if ($this->app->isLocal() || config(app.env)=='staging' ) {
+            if ($this->app->isLocal() || config('app.env')=='staging' ) {
                 return true;
             }
 
